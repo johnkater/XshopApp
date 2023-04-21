795e1930eb72a8dd4b0a39af833334b0f050694f
= <nhatkater130@gmail.com>
2023-04-22 01:04:11 +0700

    update web app

--- WebApp/Web/helloqlam/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HelloqlamConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'helloqlam'
---
+++ WebApp/Web/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Admin\Desktop\I-2023\DoAnTotNghiep\CodeRefer\2H-store-django\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
